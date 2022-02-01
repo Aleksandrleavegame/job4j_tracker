5af7bff23d271858e1114b1e591e2856d577dde5
Aleksandrleavegame <revin90@yandex.ru>
2022-02-01 20:27:40 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printinfo() {
+        System.out.println("Активность " + active);
+        System.out.println("Статус " + status);
+        System.out.println("Сообщение " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.printinfo();
+        Error error1 = new Error(true, 01, "Код работает");
+        error1.printinfo();
+        Error error2 = new Error(false, 351, "Job4j");
+        error2.printinfo();
+    }
+}
