519a2fbee602503ce7015747291ae5bc4b91cc54
Aleksandrleavegame <revin90@yandex.ru>
2022-02-02 13:07:32 +0300

    1.7. Конструктор [#173889 #258987]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -24,7 +24,7 @@ public class Error {
     public static void main(String[] args) {
         Error error = new Error();
         error.printinfo();
-        Error error1 = new Error(true, 01, "Код работает");
+        Error error1 = new Error(true, 1, "Код работает");
         error1.printinfo();
         Error error2 = new Error(false, 351, "Job4j");
         error2.printinfo();
